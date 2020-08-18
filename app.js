d700cc6196f40431099c133df6e370c80be7e254
vagrant <vagrant@homestead>
2020-08-18 04:17:21 +0000

    Removed console.logs

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ document.addEventListener('DOMContentLoaded', () => {
     function jump() {
         if(birdBottom < 500) birdBottom += 50;
         bird.style.bottom = birdBottom + 'px';
-        console.log(birdBottom);
     }
     document.addEventListener('keyup', control);
 
